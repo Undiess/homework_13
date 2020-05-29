adca5ca2c3f4067df2f3f5f7ba84a710ed47923a
Undiess <nguyen.andy67@gmail.com>
2020-05-29 23:56:56 +0930

    got server up and running

---
+++ controllers/hamburgerscontrollers.js
@@ -0,0 +1,12 @@
+var express = require("express");
+
+var router = express.Router();
+
+router.get("/",function (req,res){
+console.log("hi")
+res.render("index")
+
+});
+
+
+module.exports = router;
